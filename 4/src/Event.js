628de0b80187ca260345e1aedbbce63133da6bf9
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-17 14:40:39 -0300

    Add construction method to event class

--- 4/src/Event.js
+++ 4/src/Event.js
@@ -1,4 +1,18 @@
-export default class Event {
-  // implement your code here...
-}
+/**
+ * Simple event class implement
+ * @class Event
+ */
 
+export default class Event {
+  /**
+   * constructor description
+   * @param  {string} message Event message
+   * @param  {string} type Event type
+   * @param  {number} second Event second
+   */
+  constructor(message, type, second) {
+    this.type = type;
+    this.message = message;
+    this.second = second;
+  }
+}
