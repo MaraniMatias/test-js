092852c41df15fc83361670948d1b8d416b88dda
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-17 17:23:46 -0300

    insert and play video

--- 5/src/index.js
+++ 5/src/index.js
@@ -1,12 +1,35 @@
-const target = document.getElementById('sunmedia');
+// Docs: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+
+const target = document.getElementById("sunmedia");
+const videoElm = createVideoElement(
+  "https://vod.addevweb.com/sunmedia/demos/v/normal.mp4"
+);
+
 /**
  *
  * @param {string} src The video media file url
  * @return {HTMLVideoElement}
  */
-const videoElm = createVideoElement('https://vod.addevweb.com/sunmedia/demos/v/normal.mp4');
+function createVideoElement(src) {
+  const source = document.createElement("source");
+  source.src = src;
+
+  const videoHtmlElement = document.createElement("video");
+  const altText = "No se puede reproducir el video.";
+  videoHtmlElement.innerText = altText;
+  videoHtmlElement.muted = true;
+  videoHtmlElement.autoplay = true;
+  videoHtmlElement.controls = true;
+  videoHtmlElement.appendChild(source);
+  return videoHtmlElement;
+}
+
 /**
  * @param {HTMLDivElement} targetElm
  * @param {HTMLVideoElement} videoElm
  */
+function onInsertVideoWhenTargetIsVisible(targetElm, videoElm) {
+  targetElm.appendChild(videoElm);
+}
+
 onInsertVideoWhenTargetIsVisible(target, videoElm);
