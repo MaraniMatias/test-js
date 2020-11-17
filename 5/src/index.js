3e5bd962c716173cb6ada9cc8ffe3dfca9c6af56
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-17 18:23:04 -0300

    Add controls property to video element

--- 5/src/index.js
+++ 5/src/index.js
@@ -1,12 +1,11 @@
-// Docs: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
-
 const target = document.getElementById("sunmedia");
 const videoElm = createVideoElement(
   "https://vod.addevweb.com/sunmedia/demos/v/normal.mp4"
 );
 
 /**
- *
+ * Crea un elemento de html video con el source correspondiente
+ * Docs: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
  * @param {string} src The video media file url
  * @return {HTMLVideoElement}
  */
@@ -19,7 +18,7 @@ function createVideoElement(src) {
   videoHtmlElement.innerText = altText;
   videoHtmlElement.muted = true;
   videoHtmlElement.autoplay = true;
-  // videoHtmlElement.controls = true;
+  videoHtmlElement.controls = true;
   videoHtmlElement.appendChild(source);
   return videoHtmlElement;
 }
