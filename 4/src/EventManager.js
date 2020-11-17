6617e01dc70b9cee03a9ef8b8a0b181ed58a0862
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-17 14:43:10 -0300

    feat: show event on console log

--- 4/src/EventManager.js
+++ 4/src/EventManager.js
@@ -1,7 +1,14 @@
 export default class EventManager {
+  constructor(events) {
+    this.events = events;
+  }
+
   run() {
     // implement your code here...
     console.log("RUNNING");
+    this.events.forEach((ev) => {
+      console.log(`> At second ${ev.second}:`, ev);
+      console.log(`> At second ${ev.second}: ${JSON.stringify(ev)}`);
+    });
   }
 }
-
