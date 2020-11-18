c47ecf9ca17cec65301a2ace2e6d09a68b7b62fa
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-17 21:38:04 -0300

    add unit test for onInsertVideoWhenTargetIsVisible file

---
+++ 5/__tests__/onInsertVideoWhenTargetIsVisible.spce.js
@@ -0,0 +1,40 @@
+import chai from 'chai';
+import createVideoElement from '../src/createVideoElement';
+import onInsertVideoWhenTargetIsVisible from '../src/onInsertVideoWhenTargetIsVisible';
+
+const { assert } = chai;
+const should = chai.should();
+const src = 'https://vod.addevweb.com/sunmedia/demos/v/normal.mp4';
+
+describe('Unit test: onInsertVideoWhenTargetIsVisible', () => {
+  it('Should a function', () => {
+    assert(typeof onInsertVideoWhenTargetIsVisible === 'function');
+  });
+
+  it('Should has a source element', () => {
+    const target = document.createElement('div');
+    const videoElm = createVideoElement(src);
+    onInsertVideoWhenTargetIsVisible(target, videoElm);
+    const source = target.firstElementChild.firstElementChild;
+    should.exist(source);
+    should.equal(source.src, src);
+  });
+
+  it.skip('Should has a waiting event', () => {
+    const target = document.createElement('div');
+    const videoElm = createVideoElement(src);
+    onInsertVideoWhenTargetIsVisible(target, videoElm);
+    // should.exist(videoElm.onwaiting);
+  });
+
+  it('Check if video is playing', () => {
+    const target = document.createElement('div');
+    const videoElm = createVideoElement(src);
+    onInsertVideoWhenTargetIsVisible(target, videoElm);
+    videoElm.addEventListener('playing', () => {
+      should.exist(true);
+    });
+  });
+
+  it.skip('Remove video element after video has be finished', () => {});
+});
