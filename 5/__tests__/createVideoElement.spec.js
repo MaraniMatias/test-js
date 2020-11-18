39200a3c13ce2328d3dbed7ce9c80de0a45062a3
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-11-17 21:11:44 -0300

    add unit test for createVideoElement

--- 5/__tests__/createVideoElement.spec.js
+++ 5/__tests__/createVideoElement.spec.js
@@ -2,10 +2,8 @@ import chai from 'chai';
 import createVideoElement from '../src/createVideoElement';
 
 const src = 'https://vod.addevweb.com/sunmedia/demos/v/normal.mp4';
-// const cheerio = require('cheerio');
-
 const { assert } = chai;
-// const should = chai.should();
+const should = chai.should();
 
 describe('Unit test: CreateVideoElement', () => {
   it('Should a function', () => {
@@ -14,6 +12,23 @@ describe('Unit test: CreateVideoElement', () => {
 
   it('Return a HTML video element', () => {
     const rta = createVideoElement(src);
-    assert(typeof rta === 'object');
+    should.exist(rta);
+    should.exist(rta.src);
+    should.exist(rta.controls);
+    should.exist(rta.autoplay);
+    should.exist(rta.muted);
+  });
+
+  it('Set correct property value to autoplay and muted', () => {
+    const rta = createVideoElement(src);
+    should.equal(rta.autoplay, true, 'autoplay property should be true');
+    should.equal(rta.muted, true, 'muted property should be true');
+  });
+
+  it('Should has a source element', () => {
+    const rta = createVideoElement(src);
+    const source = rta.firstElementChild;
+    should.exist(source);
+    should.equal(source.src, src);
   });
 });
